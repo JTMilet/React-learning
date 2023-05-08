2168f13cbdc93fc97f84a7f5eceb972ae21278f0
杜康 <banshee1115@163.com>
2023-05-08 14:39:13 +0800

    react类组件实现方式

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@
  * @Author: 杜康
  * @Date: 2023-05-08 09:40:18
  * @LastEditors: 杜康
- * @LastEditTime: 2023-05-08 11:20:22
+ * @LastEditTime: 2023-05-08 14:36:50
  * @FilePath: /react-learning/src/App.js
  */
+import React from 'react'
 import './jsx-css.css'
 const name = '杜康'
 const getAge = () => {
@@ -29,37 +30,51 @@ const getHTag = type => {
   }
   return null
 }
-function App () {
-  return (
-    <div className="App">
-      <p>{name}</p>
-      <p>{getAge()}</p>
-      <p>{flag ? '真棒' : '真菜'}</p>
-      --- --- --- --- <br />
-      <ul>
-        {songs.map(song => <li key={song.id} > {song.name}</li>)}
-      </ul>
-      --- --- --- --- <br />
-      {flag ? <span>this is a span</span > : null}
-      <br />
-      {true && <span>this is another span</span>}
-      <br />
-      --- --- --- --- <br />
-      <p>{getHTag(1)}</p>
-      <p>{getHTag(2)}</p>
-      <p>{getHTag(3)}</p>
-      --- --- --- --- <br />
-      <p style={{ color: 'red', fontSize: '16px' }}>this is one style</p>
-      <p style={twoStyle}>this is two style</p>
-      <p className="jsx-css">this is jsx-css file style</p>
-      <p className={flag ? 'jsx-css' : ''}>this is jsx-css active style</p>
-    </div>
-  )
-}
 
 const twoStyle = {
   color: 'blue',
   fontSize: '24px'
 }
 
+// 函数组建
+// function App () {
+//   return (
+//     <div className="App">
+//       <p>{name}</p>
+//       <p>{getAge()}</p>
+//       <p>{flag ? '真棒' : '真菜'}</p>
+//       --- --- --- --- <br />
+//       <ul>
+//         {songs.map(song => <li key={song.id} > {song.name}</li>)}
+//       </ul>
+//       --- --- --- --- <br />
+//       {flag ? <span>this is a span</span > : null}
+//       <br />
+//       {true && <span>this is another span</span>}
+//       <br />
+//       --- --- --- --- <br />
+//       <p>{getHTag(1)}</p>
+//       <p>{getHTag(2)}</p>
+//       <p>{getHTag(3)}</p>
+//       --- --- --- --- <br />
+//       <p style={{ color: 'red', fontSize: '16px' }}>this is one style</p>
+//       <p style={twoStyle}>this is two style</p>
+//       <p className="jsx-css">this is jsx-css file style</p>
+//       <p className={flag ? 'jsx-css' : ''}>this is jsx-css active style</p>
+//     </div>
+//   )
+// }
+
+// 类组建
+class HelloComponent extends React.Component {
+  render () {
+    return (<div>this is app class component</div>)
+  }
+}
+
+function App () {
+  return (
+    <HelloComponent />
+  )
+}
 export default App
