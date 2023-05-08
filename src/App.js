8d995ca470f0078b9ded12ac9a3c8fe029502f47
杜康 <banshee1115@163.com>
2023-05-08 09:52:45 +0800

    项目基础修改
    1、去掉无用的测试文件等
    2、去掉严格模式节点

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+/*
+ * @Author: 杜康
+ * @Date: 2023-05-08 09:40:18
+ * @LastEditors: 杜康
+ * @LastEditTime: 2023-05-08 09:46:37
+ * @FilePath: /react-learning/src/App.js
+ */
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Author: 杜康
+ * @Date: 2023-05-08 09:40:18
+ * @LastEditors: 杜康
+ * @LastEditTime: 2023-05-08 09:51:11
+ * @FilePath: /react-learning/src/index.js
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+// import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // 暂时去掉严格模式节点，因为严格模式节点会影响useEffect的执行时机，会执行两次
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+)
