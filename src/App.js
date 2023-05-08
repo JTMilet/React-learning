c24a47b4b46f948833e34fa48d698946fb3f5ae5
杜康 <banshee1115@163.com>
2023-05-08 10:47:23 +0800

    jsx - 条件渲染

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@
  * @Author: 杜康
  * @Date: 2023-05-08 09:40:18
  * @LastEditors: 杜康
- * @LastEditTime: 2023-05-08 10:34:42
+ * @LastEditTime: 2023-05-08 10:46:40
  * @FilePath: /react-learning/src/App.js
  */
 const name = '杜康'
 const getAge = () => {
   return 18
 }
-const flag = true
+const flag = false
 
 const songs = [
   { id: 0, name: '第一次爱的人' },
@@ -26,7 +26,11 @@ function App () {
       <ul>
         {songs.map(song => <li key={song.id} > {song.name}</li>)}
       </ul>
-    </div >
+      --- --- --- --- <br />
+      {flag ? <span>this is a span</span > : null}
+      <br />
+      {true && <span>this is another span</span>}
+    </div>
   )
 }
 
