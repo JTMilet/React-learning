a928c04e67e1bbceae40d630b8f589283a90d26b
杜康 <banshee1115@163.com>
2023-05-08 10:02:25 +0800

    JSX - 表达式
    使用JSX构建表达式

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@
  * @Author: 杜康
  * @Date: 2023-05-08 09:40:18
  * @LastEditors: 杜康
- * @LastEditTime: 2023-05-08 09:46:37
+ * @LastEditTime: 2023-05-08 10:01:03
  * @FilePath: /react-learning/src/App.js
  */
+const name = '杜康'
+const getAge = () => {
+  return 18
+}
+const flag = true
 function App () {
   return (
     <div className="App">
+      <p>{name}</p>
+      <p>{getAge()}</p>
+      <p>{flag ? '真棒' : '真菜'}</p>
     </div>
   )
 }
